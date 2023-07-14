35742edb4224e9e44be9d50bafb6ba1d5cc99919
debugging-bug <139357379+debugging-bug@users.noreply.github.com>
2023-07-14 09:05:54 +0000

    more changes kill me now

--- mlfromscratch/deep_learning/__init__.py
+++ mlfromscratch/deep_learning/__init__.py
@@ -1,1 +1,3 @@
-from neural_network import NeuralNetwork
+import sys    
+print("In module products sys.path[0], __package__ ==", sys.path[1], 'mlfromscratch')
+    # from mlfromscratch.neural_network import NeuralNetwork
